3541c06e7f4acbdc54f4e0cb36cd860df1899958
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-25 15:10:46 +0300

    chage to es6

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  for (const idx of array) {
+    const value = idx;
+    idx = appendString + value;
   }
 
   return array;
