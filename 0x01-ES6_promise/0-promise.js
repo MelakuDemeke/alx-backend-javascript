26286b1510b0c7c022519f28a2df502d44b0536b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-26 12:17:18 +0300

    include bracket

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => { });
+export default function getResponseFromAPI() {
+  return (new Promise((resolve, reject) => { }));
 }
