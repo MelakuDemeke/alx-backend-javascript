7bbfe119e2f2c49ac0de0ca6461b1a1b4f78878a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-26 15:46:21 +0300

    remove the promise

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    if (denominator === 0) {
-      reject(new Error('Cannot divide by 0'));
-    } else {
-      resolve(numerator / denominator);
-    }
-  });
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    return (numerator / denominator);
+  }
 }
