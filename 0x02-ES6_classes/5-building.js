6b139857a6837a6021c77b8a0ac04178b308af72
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-29 14:39:45 +0300

    make class to define evacuationWarningMessage

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,9 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
