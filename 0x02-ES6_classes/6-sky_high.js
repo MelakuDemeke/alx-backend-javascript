1036d1cd499b7540ae8f57fd7946356ca96825b0
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-29 14:45:54 +0300

    define constructor for sqft and floors

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,5 +1,8 @@
 import Building from "./5-building";
 
 export default class SkyHighBuilding extends Building {
-  
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 }
