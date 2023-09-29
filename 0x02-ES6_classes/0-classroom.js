1bdc2f87a1306e7869af57c88cb220236dca644a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-29 13:48:44 +0300

    make the var names plular

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
 export default class ClassRoom {
-  constructor(maxStudentSize) {
-    this._maxStudentSize = maxStudentSize;
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
