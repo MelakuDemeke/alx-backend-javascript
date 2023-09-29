22f911e58aabe99166bc72fde8085d1596a95074
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-29 13:47:37 +0300

    push new classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,10 @@
 import {ClassRoom} from './0-classroom';
 
 export default function initializeRooms() {
-  
+  const rooms = [];
+  rooms.push(new ClassRoom(19));
+  rooms.push(new ClassRoom(20));
+  rooms.push(new ClassRoom(34));
+
+  return rooms;
 }
