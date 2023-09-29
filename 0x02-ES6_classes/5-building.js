fdc3b2cfad0cfefd77f49f605e4382db434c9061
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-29 14:38:13 +0300

    get and set defined for sqft

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,4 +2,12 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
 }
