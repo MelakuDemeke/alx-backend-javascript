b805ddc6ccb270833300a67778f60c5eced34660
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-02 14:01:09 +0300

    remove array check

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,4 @@
 const getStudentsByLocation = (studentList, location) => {
-  if (!Array.isArray(studentList)) {
-    return [];
-  }
   return studentList.filter((student) => student.location === location);
 }
 
