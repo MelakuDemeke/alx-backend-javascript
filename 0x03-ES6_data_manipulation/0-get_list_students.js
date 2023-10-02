1ecf7284783b363f0f77457fc95668cc9939046c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-02 11:41:37 +0300

    follow es6

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,9 @@
-export default function getListStudents() {
+const getListStudents= () => {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 }
+
+export default getListStudents;
