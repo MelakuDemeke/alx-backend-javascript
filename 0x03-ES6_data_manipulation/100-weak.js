48fd9118ed8f043a070bff4a47fe41ac8b8ff01f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 14:35:31 +0300

    api call count

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,5 +1,13 @@
-export const WeakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-export const queryAPI = (endpoint) => {
-  
-};
+export function queryAPI(endpoint) {
+  let callCount = weakMap.get(endpoint) || 0;
+
+  callCount += 1;
+
+  weakMap.set(endpoint, callCount);
+
+  if (callCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
