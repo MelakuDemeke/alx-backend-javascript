a1d4d4579a18f7b8498fa3b49fe215ea655fc4e7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 14:16:27 +0300

    return list

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,5 +1,9 @@
-const groceriesList = () => {
-
-};
+const groceriesList = () => new Map([
+  ['Apples', 10],
+  ['Tomatoes', 10],
+  ['Pasta', 1],
+  ['Rice', 1],
+  ['Banana', 5],
+]);
 
 export default groceriesList;
