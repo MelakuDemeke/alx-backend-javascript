9608168675bc410822d0352dfb88858ae4c6da9e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 14:04:59 +0300

    check for if length of startstring is 0

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,12 @@
-const cleanSet = (set, startString) => [...set]
-  .filter((item) => item !== undefined)
-  .map((item) => (item.startsWith(startString) ? item.slice(startString.length) : ''))
-  .join('-');
+const cleanSet = (set, startString) => {
+  if (!startString || startString.length === 0) {
+    return '';
+  }
+
+  return [...set]
+    .filter((item) => item !== undefined)
+    .map((item) => (item.startsWith(startString) ? item.slice(startString.length) : ''))
+    .join('-');
+};
 
 export default cleanSet;
