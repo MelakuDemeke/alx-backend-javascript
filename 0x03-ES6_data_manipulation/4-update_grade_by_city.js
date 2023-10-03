4e41b2b5779647e5f2e7b7e66c9ab9a85e18c0eb
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 10:41:52 +0300

    check if student and newGrade are array

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,5 +1,7 @@
-const updateStudentGradeByCity = () => {
-
+const updateStudentGradeByCity = (student, city, newGrade) => {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
 }
 
 export default updateStudentGradeByCity;
