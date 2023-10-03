e143f259a341c934df6d8bde139efdb59ac7b596
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 10:44:24 +0300

    update grade by city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,12 @@
-const updateStudentGradeByCity = (student, city, newGrade) => {
+const updateStudentGradeByCity = (students, city, newGrades) => {
   if (!Array.isArray(students) || !Array.isArray(newGrades)) {
     return [];
   }
-}
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item2 = newGrades.find((student2) => student.id === student2.studentId);
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
+  });
+  return cityStudents;
+};
 
 export default updateStudentGradeByCity;
