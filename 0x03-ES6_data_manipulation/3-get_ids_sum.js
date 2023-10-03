70103963743672db7407c76607174121c9e85b64
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 10:30:23 +0300

    return sum of the id using reducer

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,6 @@
-const getStudentIdsSum = () => {
-
+const getStudentIdsSum = (studentList) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue.id;
+  return studentList.reduce(reducer, 0);
 }
 
 export default getStudentIdsSum;
