7e11a5bf7a01e916d1b005d27b87c64fb637a3bc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 10:55:43 +0300

    create buffer then int8array then create typearray

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,11 @@ const createInt8TypedArray = (length, position, value) => {
   if (position < 0 || position > length - 1) {
     throw new Error('Position outside range');
   }
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
 
+  return new DataView(buffer);
 }
 
 export default createInt8TypedArray;
