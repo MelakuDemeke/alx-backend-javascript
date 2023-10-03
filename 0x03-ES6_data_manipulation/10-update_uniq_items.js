b941cb841e7cd60cd62cc5fbc344e8bb57c5d8df
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-03 14:23:07 +0300

    handle unique items in Map by updating their value

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,5 +1,13 @@
-const updateUniqueItems = () => {
-
+const updateUniqueItems = (map) => {
+  if (map instanceof Map) {
+    map.forEach((value, key, map) => {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    });
+    return map;
+  }
+  throw new Error('Cannot process');
 };
 
 export default updateUniqueItems;
