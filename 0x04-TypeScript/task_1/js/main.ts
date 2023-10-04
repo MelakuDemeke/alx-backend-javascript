d6d7eb0fcd31e19d1bd23e460b245e002f683e78
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-04 14:42:24 +0300

    define student class constructor

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -36,6 +36,12 @@ interface IStudentClass {
 class StudentClass implements IStudentClass {
   private _firstName: string;
   private _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
 }
 
 
