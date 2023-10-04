c70b5e381e85b5956220e58934bdb0bd4ab04cec
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-04 14:04:04 +0300

    create two students

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -5,3 +5,17 @@ export interface Student {
   location: string;
 }
 
+const student1: Student = {
+  firstName: "Melaku",
+  lastName: "Demeke",
+  age: 26,
+  location: "Addis ababa, Ethiopia",
+};
+const student2: Student = {
+  firstName: "Getnet",
+  lastName: "legese",
+  age: 22,
+  location: "Aseb Erithrea",
+};
+
+
