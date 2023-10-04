8c1519f704620c4e37fe6e2698e01bca8e6f7c66
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-04 14:32:36 +0300

    add remaining property

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,8 @@
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [index:string]: any;
 }
