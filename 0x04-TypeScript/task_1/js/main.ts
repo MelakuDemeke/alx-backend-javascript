840f2d5de5900b2dff97273e4836cf6340e98881
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-04 14:39:23 +0300

    add comments and define printTeacher with its type

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,13 +1,22 @@
+// Define a Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [index:string]: any;
+  [key: string]: any; // Allow additional properties with any value
 }
 
-export interface Directors extends Teacher {
+// Define a Directors interface extending Teacher
+interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+// Define a function type for printing a teacher's name
+type PrintTeacherFunction = (firstName: string, lastName: string) => string;
+
+// Implement a function to print a teacher's name
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
