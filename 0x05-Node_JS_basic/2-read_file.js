704a2ba870701533970bf4f65d55b780884931a2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-20 16:21:39 +0300

    Fix file path and initialize variables

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 
 function countStudents (path) {
+  // Check if the file exists
   if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
-  const fileContent = fs.readFileSync(dataPath, 'utf-8').trim();
+
+  // Read and process the file contents
+  const fileContent = fs.readFileSync(path, 'utf-8').trim();
   const fileLines = fileContent.split('\n');
+
+  // Initialize Variables
+  const studentGroups = {};
+  const dbFieldNames = fileLines[0].split(',');
+  const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 }
 
 module.exports = countStudents;
