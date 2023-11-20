306eda033825de5226d0cbf0987ceab1ad8c7aa7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-20 12:06:50 +0300

    read input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,8 @@
 
 console.log('Welcome to Holberton School, what is your name?');
-
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    console.log(`Your name is: ${name}`);
+  }
+});
