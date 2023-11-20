4e84cee8e5fb3ef9cbc9decbaeb39b870986efd0
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-20 16:17:51 +0300

    Add file reading functionality

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,6 +4,8 @@ function countStudents (path) {
   if (!fs.existsSync(path) || !fs.statSync(path).isFile()) {
     throw new Error('Cannot load the database');
   }
+  const fileContent = fs.readFileSync(dataPath, 'utf-8').trim();
+  const fileLines = fileContent.split('\n');
 }
 
 module.exports = countStudents;
