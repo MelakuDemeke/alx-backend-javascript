7f9d72f69f625a080a876f4857215c34396a6718
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-20 12:11:23 +0300

    Add end event listener to close the software

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,3 +5,7 @@ process.stdin.on('readable', () => {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
