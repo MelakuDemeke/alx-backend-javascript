647cc4471efe3856057417d505008cd9fec6749d
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-20 16:10:31 +0300

    Add file existence check in countStudents function

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,6 +4,9 @@ function countStudents (path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
+  if(!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
