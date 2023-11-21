26dae0c27e96e4ca3a6308063bd728af77f35e3a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 15:28:27 +0300

    define class and two static methods

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,11 @@
+import readDatabase from '../utils';
+
+class StudentController {
+  static getAllStudents(request, response) {
+
+  }
+
+  static getAllStudentsByMajor(request, response) {
+
+  }
+}
