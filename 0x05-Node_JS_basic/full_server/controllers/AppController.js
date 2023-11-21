6fd265851785c920ddbe1cba028dedbcd4577ecf
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 17:30:45 +0300

    eslint fix

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,12 +1,11 @@
-
 /**
  * Controller class for handling application routes.
  * @author Melaku Demeke <https://github.com/MelakuDemeke>
  */
 class AppController {
-	static getHomepage (request, response) {
-		response.status(200).send('Hello Holberton School!');
-	}
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
 }
 
 export default AppController;
