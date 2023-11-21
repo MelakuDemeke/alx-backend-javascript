56ebefd60ef22633e4c4da7fa7046b8074e893b4
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 16:15:44 +0300

    define the index file

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,5 +1,15 @@
-const mapRoutes = (app) => {
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
 
-}
+/**
+ * Maps routes to the app.
+ * 
+ * @param {Object} app - The Express app object.
+ */
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
 
 export default mapRoutes;
