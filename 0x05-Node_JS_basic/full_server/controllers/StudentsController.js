cdcc104dbab59de89eb186eb1f19b739e78ac85b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 15:51:07 +0300

    Fix error handling in StudentsController

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -12,6 +12,9 @@ class StudentController {
           responseParts.push(`Number of students in ${field}: ${group.length}. List: ${group.map((student) => student.firstname).join(', ')}`);
         }
         res.status(200).send(responseParts.join('\n'));
+      })
+      .catch((err) => {
+        res.status(500).send(err instanceof Error ? err.message : err.toString());
       });
   }
 
