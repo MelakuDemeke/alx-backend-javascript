3bf4cbfcda5657dfea9cc327735488eb0aa68e7e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 15:19:48 +0300

    add route handler

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,12 @@
+
+/**
+ * Controller class for handling application routes.
+ * @author Melaku Demeke <https://github.com/MelakuDemeke>
+ */
+class AppController {
+	static getHomepage (request, response) {
+		response.status(200).send('Hello Holberton School!');
+	}
+}
+
+exports.default = AppController;
