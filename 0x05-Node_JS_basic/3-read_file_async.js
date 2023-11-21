ffb1905f49b9fcb0fa88d87a5688f81ffc32c378
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 11:28:56 +0300

    add docstring

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,2 +1,8 @@
 const fs = require('fs');
 
+/**
+ * Counts the number of students in a CSV file and displays the results
+ * @param {string} path - The path to the database file
+ * @returns {Promise} - The number of students in the database
+ * @author Melaku Demeke <https://github.com/MelakuDemeke>
+ */
