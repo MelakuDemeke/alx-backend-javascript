6259cb3add958a27947c8f760e372993a3e67c33
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 11:46:34 +0300

    copy student group reader for 2-file read

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -18,7 +18,24 @@ const countStudents = (path) => new Promise((resolve, reject) => {
       const studentGroups = {};
       const dbFieldNames = fileLines[0].split(',');
       const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+
+      for (const line of fileLines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
     
+        if (!studentGroups[field]) {
+          studentGroups[field] = [];
+        }
+    
+        const studentEntry = Object.fromEntries(
+          studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]),
+        );
+    
+        studentGroups[field].push(studentEntry);
+      }
+
+      
     }
   });
 });
