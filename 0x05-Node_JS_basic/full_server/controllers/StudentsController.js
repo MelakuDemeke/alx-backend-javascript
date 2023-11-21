0adc82d84e66cb6be2e640cbb42feb5e279eddb3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 15:50:33 +0300

    Refactor getAllStudents method to sort student groups by field and display the number of students in each group

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,15 +1,21 @@
 import readDatabase from '../utils';
 
 class StudentController {
-  static getAllStudents(request, response) {
+  static getAllStudents(req, res) {
     const dataPath = process.argv[2] || '';
     readDatabase(dataPath)
       .then((studentGroups) => {
         const responseParts = ['List of students:'];
+        const compareIgnoreCase = (a, b) => a.toLowerCase().localeCompare(b.toLowerCase());
+        for (const [field, group] of Object.entries(studentGroups).sort((a,
+          b) => compareIgnoreCase(a[0], b[0]))) {
+          responseParts.push(`Number of students in ${field}: ${group.length}. List: ${group.map((student) => student.firstname).join(', ')}`);
+        }
+        res.status(200).send(responseParts.join('\n'));
       });
   }
 
-  static getAllStudentsByMajor(request, response) {
+  static getAllStudentsByMajor(req, res) {
 
   }
 }
