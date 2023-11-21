73e3574fc3a36c452927a5730cd53983ff3e5dfd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 14:54:52 +0300

    copy count Studetns function

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,6 +1,50 @@
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
 
-const app = express()
-const PORT = 1245
+const app = express();
+const PORT = 1245;
 const DB_FILE = process.argv[2] || '';
+
+const countStudents = (dataPath) => new Promise((resolve, reject) => {
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+    return;
+  }
+
+  fs.readFile(dataPath, 'utf-8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const reportParts = [];
+    const fileLines = data.trim().split('\n');
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, -1);
+
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, -1);
+      const field = studentRecord[studentRecord.length - 1];
+
+      if (!studentGroups[field]) {
+        studentGroups[field] = [];
+      }
+
+      const studentEntries = studentPropNames.map(
+        (propName, idx) => [propName, studentPropValues[idx]]
+      );
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
+
+    const totalStudents = Object.values(studentGroups).reduce((pre, cur) => pre + cur.length, 0);
+    reportParts.push(`Number of students: ${totalStudents}`);
+
+    for (const [field, group] of Object.entries(studentGroups)) {
+      reportParts.push(`Number of students in ${field}: ${group.length}. List: ${group.map((student) => student.firstname).join(', ')}`);
+    }
+
+    resolve(reportParts.join('\n'));
+  });
+});
