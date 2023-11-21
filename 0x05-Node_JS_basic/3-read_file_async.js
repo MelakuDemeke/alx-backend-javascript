cc44a8ead898e0faba4b60fe60fe837e8273acb7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 11:50:10 +0300

    resolve to true

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -23,19 +23,27 @@ const countStudents = (path) => new Promise((resolve, reject) => {
         const studentRecord = line.split(',');
         const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
         const field = studentRecord[studentRecord.length - 1];
-    
+
         if (!studentGroups[field]) {
           studentGroups[field] = [];
         }
-    
+
         const studentEntry = Object.fromEntries(
-          studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]),
+          studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]])
         );
-    
+
         studentGroups[field].push(studentEntry);
       }
 
-      
+      const totalStudents = Object.values(studentGroups)
+        .reduce((total, group) => total + group.length, 0);
+      console.log(`Number of students: ${totalStudents}`);
+
+      for (const [field, group] of Object.entries(studentGroups)) {
+        const studentNames = group.map((student) => student.firstname).join(', ');
+        console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+      }
+      resolve(true);
     }
   });
 });
