61a3fea97114e3c89dfe36beec52682e8647f19a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 16:19:28 +0300

    add server code

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import mapRoutes from './routes';
+
+const app = express();
+const PORT = 1245;
+
+mapRoutes(app);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
