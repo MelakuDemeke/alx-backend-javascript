9c48df49696cd6d3216b7d388ea7bf47acd8669c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 15:04:26 +0300

    Add server listening functionality

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -52,3 +52,11 @@ const countStudents = (dataPath) => new Promise((resolve, reject) => {
 app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
