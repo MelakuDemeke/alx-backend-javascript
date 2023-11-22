9dfcc46fd23e7361b44dc46f8637b44e4aa7e7c9
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 15:47:39 +0300

    Add divide functionality to calculateNumber function

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -14,6 +14,13 @@ const calculateNumber = (type, a, b) => {
     return Math.round(a) - Math.round(b);
   }
 
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
+
   return 0;
 };
 
