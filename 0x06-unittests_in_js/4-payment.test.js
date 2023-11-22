006139b84449850d627b2247405020f5c935940e
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 16:50:53 +0300

    Fix calculateNumberStub and add consoleSpy in sendPaymentRequestToApi test

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -5,13 +5,17 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   let calculateNumberStub;
+  let consoleSpy;
 
   beforeEach(() => {
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    consoleSpy = sinon.spy(console, 'log');
   });
 
   afterEach(() => {
     calculateNumberStub.restore();
+    consoleSpy.restore();
   });
 
   it('should use calculateNumber method of Utils with correct arguments', () => {
@@ -22,5 +26,8 @@ describe('sendPaymentRequestToApi', () => {
 
     expect(calculateNumberStub.calledOnce).to.be.true;
     expect(calculateNumberStub.calledWithExactly('SUM', amount, tax)).to.be.true;
+
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWithExactly('The total is: 10')).to.be.true;
   });
 });
