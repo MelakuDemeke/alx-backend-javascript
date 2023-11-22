6ee5b93aa748510c7fa237faedd39d33e6b8f955
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 15:56:50 +0300

    add test for addiotn

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,21 +3,42 @@ const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
   describe('Test Addition', () => {
-    it('', () => {
-      assert.strictEqual(calculateNumber('SUM', , ), );
+    it('should add two positive integers correctly', () => {
+      assert.strictEqual(calculateNumber('SUM', 2, 3), 5);
+    });
+  
+    it('should add two negative integers correctly', () => {
+      assert.strictEqual(calculateNumber('SUM', -2, -3), -5);
+    });
+  
+    it('should add a positive integer and a negative integer correctly', () => {
+      assert.strictEqual(calculateNumber('SUM', 2, -3), -1);
+    });
+  
+    it('should add two floats correctly', () => {
+      assert.strictEqual(calculateNumber('SUM', 2.7, 3.2), 6);
+    });
+  
+    it('should add two negative floats correctly', () => {
+      assert.strictEqual(calculateNumber('SUM', -2.7, -3.2), -6);
+    });
+  
+    it('should add a positive float and a negative float correctly', () => {
+      assert.strictEqual(calculateNumber('SUM', 2.7, -3.2), 0);
+    });
+  
+    it('should return 0 for two zeros', () => {
+      assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
     });
   });
 
   describe('Test Subtraction', () => {
-    it('', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', , ), );
-    });
+    
   });
 
-  describe('Test Subtraction', () => {
-    it('', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', , ), );
-    });
+  describe('Test Division', () => {
+    
   });
+
 
 });
