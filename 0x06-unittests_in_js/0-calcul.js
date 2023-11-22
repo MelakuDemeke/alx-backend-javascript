6ef552b6453b8d520893d6c1d882f611cb5640f6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 14:59:37 +0300

    define function to calc number

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,12 @@
+/**
+ * Adds two numbers.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+function calculateNumber(a, b) {
+    return a + b;
+}
+
+module.exports = calculateNumber;
