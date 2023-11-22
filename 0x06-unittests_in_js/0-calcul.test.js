3298cddb626f606e5444d3344b2fe5c870a6a300
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 15:29:10 +0300

    Add unit test for calculateNumber for two +v int

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,2 +1,10 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+
+	it('should add two positive integers correctly', () => {
+		assert.strictEqual(calculateNumber(2, 3), 5);
+	});
+
+});
