2b96237798541218b67102e2e9a3c4e62bb1bc51
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 15:42:17 +0300

    import calc code

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,10 @@
+/**
+ * Adds two numbers.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+
+module.exports = calculateNumber;
