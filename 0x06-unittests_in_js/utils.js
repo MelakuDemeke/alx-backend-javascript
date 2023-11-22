9cb5ae063116177684ab570f5f79ecb66479cfb8
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 16:34:46 +0300

    def ine calc

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,5 +1,22 @@
 const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
 
-}
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
 
-module.exports = Utils
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+
+    return 0;
+  },
+};
+
+module.exports = Utils;
