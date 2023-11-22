9e8ce64fdcf999e5024910a2c710e9e31b0aebc6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 16:25:51 +0300

    Refactor unit tests to use chai expect

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,94 +1,94 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber', () => {
   describe('test Addition', () => {
     it('should add two positive integers correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', 2, 3), 5);
+      expect(calculateNumber('SUM', 2, 3)).to.equal(5);
     });
 
     it('should add two negative integers correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', -2, -3), -5);
+      expect(calculateNumber('SUM', -2, -3)).to.equal(-5);
     });
 
     it('should add a positive integer and a negative integer correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', 2, -3), -1);
+      expect(calculateNumber('SUM', 2, -3)).to.equal(-1);
     });
 
     it('should add two floats correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', 2.7, 3.2), 6);
+      expect(calculateNumber('SUM', 2.7, 3.2)).to.equal(6);
     });
 
     it('should add two negative floats correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', -2.7, -3.2), -6);
+      expect(calculateNumber('SUM', -2.7, -3.2)).to.equal(-6);
     });
 
     it('should add a positive float and a negative float correctly', () => {
-      assert.strictEqual(calculateNumber('SUM', 2.7, -3.2), 0);
+      expect(calculateNumber('SUM', 2.7, -3.2)).to.equal(0);
     });
 
     it('should return 0 for two zeros', () => {
-      assert.strictEqual(calculateNumber('SUM', 0, 0), 0);
+      expect(calculateNumber('SUM', 0, 0)).to.equal(0);
     });
   });
 
   describe('test Subtraction', () => {
     it('should subtract two positive integers correctly', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 5, 3), 2);
+      expect(calculateNumber('SUBTRACT', 5, 3)).to.equal(2);
     });
 
     it('should subtract two negative integers correctly', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', -2, -3), 1);
+      expect(calculateNumber('SUBTRACT', -2, -3)).to.equal(1);
     });
 
     it('should subtract a positive integer and a negative integer correctly', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 2, -3), 5);
+      expect(calculateNumber('SUBTRACT', 2, -3)).to.equal(5);
     });
 
     it('should subtract two floats correctly (rounded)', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 3.5, 1.2), 3);
+      expect(calculateNumber('SUBTRACT', 3.5, 1.2)).to.equal(3);
     });
 
     it('should subtract two negative floats correctly (rounded)', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', -2.7, -1.2), -2);
+      expect(calculateNumber('SUBTRACT', -2.7, -1.2)).to.equal(-2);
     });
 
     it('should subtract a positive float and a negative float correctly (rounded)', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 5.7, -3.2), 9);
+      expect(calculateNumber('SUBTRACT', 5.7, -3.2)).to.equal(9);
     });
 
     it('should return 0 for two equal numbers', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 3, 3), 0);
+      expect(calculateNumber('SUBTRACT', 3, 3)).to.equal(0);
     });
   });
 
   describe('test Division', () => {
     it('should divide two positive integers correctly', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 6, 3), 2);
+      expect(calculateNumber('DIVIDE', 6, 3)).to.equal(2);
     });
 
     it('should divide two negative integers correctly', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -9, -3), 3);
+      expect(calculateNumber('DIVIDE', -9, -3)).to.equal(3);
     });
 
     it('should divide a positive integer by a negative integer correctly', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 10, -2), -5);
+      expect(calculateNumber('DIVIDE', 10, -2)).to.equal(-5);
     });
 
     it('should divide two floats correctly (rounded)', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 5.5, 2.2), 3);
+      expect(calculateNumber('DIVIDE', 5.5, 2.2)).to.equal(3);
     });
 
     it('should divide two negative floats correctly (rounded)', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', -8.4, -2), 4);
+      expect(calculateNumber('DIVIDE', -8.4, -2)).to.equal(4);
     });
 
     it('should divide a positive float by a negative float correctly (rounded)', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 7.2, -1.8), -3.5);
+      expect(calculateNumber('DIVIDE', 7.2, -1.8)).to.equal(-3.5);
     });
 
     it('should return "Error" when dividing by 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 5, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 5, 0)).to.equal('Error');
     });
   });
 });
