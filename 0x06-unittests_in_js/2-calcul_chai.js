3e062495ee6635d22ce7430990625e5910e22b2b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 16:10:19 +0300

    copy from previous

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,27 @@
+/**
+ * Adds two numbers.
+ *
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of the two numbers.
+ */
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+
+  if (type === 'DIVIDE') {
+    if (Math.round(b) === 0) {
+      return 'Error';
+    }
+    return Math.round(a) / Math.round(b);
+  }
+
+  return 0;
+};
+
+module.exports = calculateNumber;
