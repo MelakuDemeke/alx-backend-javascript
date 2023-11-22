3eb396133d1d189de788ace42470f7dada123372
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 15:53:10 +0300

    ADD TEPLATE

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('Test Addition', () => {
+    it('', () => {
+      assert.strictEqual(calculateNumber('SUM', , ), );
+    });
+  });
+
+  describe('Test Subtraction', () => {
+    it('', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', , ), );
+    });
+  });
+
+  describe('Test Subtraction', () => {
+    it('', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', , ), );
+    });
+  });
+
+});
