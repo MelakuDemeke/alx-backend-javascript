7ec351858f29ae5941079c29d3260f7332f8e9c7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 16:39:48 +0300

    Add sendPaymentRequestToApi function to handle payment requests

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,5 +1,11 @@
 const Utils = require('./utils');
 
+/**
+ * Sends a payment request to the API.
+ * @param {number} totalAmount - The total amount of the payment.
+ * @param {number} totalShipping - The total shipping cost.
+ * @returns {void}
+ */
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total is: ${total}`);
