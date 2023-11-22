b7f5397dec0c9ca97aaea0f3f44d436409c66653
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 16:01:12 +0300

    Add subtraction test cases to calculateNumber function

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -33,7 +33,33 @@ describe('calculateNumber', () => {
   });
 
   describe('Test Subtraction', () => {
-    
+    it('should subtract two positive integers correctly', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 5, 3), 2);
+    });
+  
+    it('should subtract two negative integers correctly', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', -2, -3), 1);
+    });
+  
+    it('should subtract a positive integer and a negative integer correctly', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2, -3), 5);
+    });
+  
+    it('should subtract two floats correctly (rounded)', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 3.5, 1.2), 3);
+    });
+  
+    it('should subtract two negative floats correctly (rounded)', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', -2.7, -1.2), -2);
+    });
+  
+    it('should subtract a positive float and a negative float correctly (rounded)', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 5.7, -3.2), 9);
+    });
+  
+    it('should return 0 for two equal numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 3, 3), 0);
+    });
   });
 
   describe('Test Division', () => {
