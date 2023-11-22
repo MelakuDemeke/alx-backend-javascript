7b56d73664d4e9ecbdc065384f09e4634d9f2b50
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-22 15:09:04 +0300

    Refactor calculateNumber function to use arrow function syntax

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -5,8 +5,6 @@
  * @param {number} b - The second number.
  * @returns {number} The sum of the two numbers.
  */
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
+const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
 
 module.exports = calculateNumber;
