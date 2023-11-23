8af8686b68fb195bfb143430426ffef8ee0e8c9c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-23 10:12:53 +0300

    Update import statement in calculateNumber test file

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai.test');
 
 describe('calculateNumber', () => {
   describe('test Addition', () => {
