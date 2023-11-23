749ea4b1337eefb1e590ce4eb9ef1a16372f5f2a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-23 10:52:52 +0300

    Update import statement in payment test file

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,7 +1,7 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi', () => {
   let calculateNumberStub;
