a1ffeb7a6c9c1dc995ddeac5024ad5b32d7e8a12
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-23 11:02:37 +0300

    Fix test cases in 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,20 +1,29 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
-const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./5-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should use calculateNumber method of Utils with correct arguments', () => {
-    const utilsSpy  = sinon.spy(console);
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+  let utilsSpy;
 
-    calculateNumberStub.returns(10);
+  beforeEach(() => {
+    if (!utilsSpy) {
+      utilsSpy = sinon.spy(console);
+    }
+  });
+
+  afterEach(() => {
+    utilsSpy.log.resetHistory();
+  });
+
+  it('The total is: 120" to the console when invoking `sendPaymentRequestToApi(100, 20)', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
-    expect(calculateNumberStub.callCount).to.be.equal(1);
-    expect(utilsSpy .log.calledWith('The total is: 10')).to.be.true;
-    expect(utilsSpy .log.callCount).to.be.equal(1);
-    calculateNumberStub.restore();
-    utilsSpy .log.restore();
+    expect(utilsSpy.log.calledWith('The total is: 120')).to.be.true;
+    expect(utilsSpy.log.calledOnce).to.be.true;
+  });
+
+  it('The total is: 20" to the console when invoking `sendPaymentRequestToApi(10, 10)', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(utilsSpy.log.calledWith('The total is: 20')).to.be.true;
+    expect(utilsSpy.log.calledOnce).to.be.true;
   });
 });
