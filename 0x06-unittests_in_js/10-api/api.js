75b3fb4cbb032c9934a1f6e1f85ebc167ea5f860
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-23 13:32:32 +0300

    Add endpoint for available payments

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -11,6 +11,15 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${req.params.id}`);
 });
 
+app.get('/available_payments', (req, res) => {
+  res.json({
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`API served on localhost port ${PORT}`);
 });
