97a11f914aac3df4120d560235696c461b251db7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-23 11:54:35 +0300

    Add unit test for getPaymentTokenFromAPI function

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,4 +1,12 @@
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-
+describe('getPaymentTokenFromAPI', () => {
+  it('Retrieve payment token from the API where success == true', (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((res) => {
+        expect(res).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      });
+  });
+});
